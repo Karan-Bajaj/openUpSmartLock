1a4642d96dbc679b3a83bef0710f9414ee51046b
Karan Bajaj <karan3663@gmail.com>
2018-03-12 19:24:47 -0700

    input piezo buzzer test

--- mainActivity/mainActivity.ino
+++ mainActivity/mainActivity.ino
@@ -1,8 +1,27 @@
+int analogPin = 0  ;     // potentiometer wiper (middle terminal) connected to analog pin 
+                       // outside leads to ground and +5V
+int val = 0;           // variable to store the value read
+
+void setup()
+{
+  Serial.begin(9600);              //  setup serial
+}
+
+void loop()
+{
+  val = analogRead(analogPin);     // read the input pin
+  Serial.println(val);             // debug value
+  delay(100);
+}
+
+
+
+/*
 #include <Servo.h>
 
 Servo myservo; 
-int pos=0;
 
+int pos=0;
 
 void setup() {
   
@@ -11,14 +30,13 @@ void setup() {
 }
 
 void loop() {
-  
   myservo.write(pos);
-  
-  if(pos==150)
-    pos=-30;
-  
-  delay(1000);
-  pos= pos+30;
-  
-  
+  delay(700);
+  pos+=10;
+  myservo.write(180-pos);  
+  delay(700);
+  pos+=10;
+  if(pos>=90)
+    pos=0;
 }
+*/

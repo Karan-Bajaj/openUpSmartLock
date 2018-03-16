d8562608c00542ac9c0e2977d96871f2f0ebcd10
Karan Bajaj <karan3663@gmail.com>
2018-03-15 22:20:00 -0700

    Backup mar15

--- mainActivity/mainActivity.ino
+++ mainActivity/mainActivity.ino
@@ -8,6 +8,8 @@ int piezoPin=3;
 int servoPin=9;
 
 
+const int minKnockSound= 70;
+
 void setup() {
 
   Serial.begin(9600); //FOR BUTTON
@@ -33,11 +35,109 @@ void lockIt()
   delay(200);
 }
 
+
+/*
+
+int storePassword(int &pswrdAr, int cnt)
+{
+  if(pswrdAr[0]!=0)
+  {
+    timeStart=millis(); 
+  }
+
+  int val=analogRead(piezoPin);
+  
+  while(val<minKnockSound)
+  {
+    val=analogRead(piezoPin);
+  }
+  while(val>minKnockSound)
+  {
+    val=analogRead(piezoPin);
+  }
+
+  int timeEnd=millis();
+  
+  pswrdAr[cnt]=timeEnd-timeStart;
+
+  
+}*/
+
+
 void loop()
 {
   bool readState=digitalRead(2);
   int val = analogRead(piezoPin);
   Serial.println(val);
+
+  if (val>minKnockSound)
+    delay(500);
+
+  //int *pswrdAr = new int[10];   Extra Feature: Dynamic memmory allocation if size goes beyond specified
+  
+  int pswrdAr[10]={0};
+  int cnt=0;
+  int timeStart=0;
+  int timeEnd=0;
+
+  if(readState)
+  {
+    Serial.println("ok");
+    delay(1000);
+    readState=digitalRead(2);
+
+    
+
+    if(!readState)  //User attempts knocking pattern to open lock
+    {
+      
+    }
+    
+    else
+    {
+      Serial.println("ok");
+      while(readState)
+      {
+    
+        while(val<minKnockSound)  //Rising edge of sound wave signal
+        {
+          val=analogRead(piezoPin);
+          Serial.println("In1");
+        }
+    
+        while(val>minKnockSound)  //Falling edge of sound wave signal
+        {
+          val=analogRead(piezoPin);
+          Serial.println("In2");
+        }
+        timeStart=millis();  
+
+        val=analogRead(piezoPin);
+        
+        while(val<minKnockSound)
+        {
+          val=analogRead(piezoPin);
+          Serial.println("In3");
+        }
+        
+        Serial.println("In4");
+        delay(2000);
+        int timeEnd=millis();
+        pswrdAr[cnt]=timeEnd-timeStart; 
+    
+        cnt++;
+       
+      }
+      
+    }
+    
+  }
+
+  for(int i=0; i<10;i++)
+  {
+    Serial.println(pswrdAr[i]);
+  }
+  /*
   if (readState||val>80)
   {
     Serial.println("Reading");
@@ -49,11 +149,12 @@ void loop()
   
   else 
     Serial.println("Ignoring");
-  delay(1);
-
-
   
+  
+  delay(1);
+  */
 }
+
 
 
 
@@ -80,8 +181,8 @@ void loop() {
 
 
 /*
-#FOR Piezo
-int analogPin = 0  ;     // potentiometer wiper (middle terminal) connected to analog pin 
+//#FOR Piezo
+int analogPin = 3  ;     // potentiometer wiper (middle terminal) connected to analog pin 
                        // outside leads to ground and +5V
 int val = 0;           // variable to store the value read
 
@@ -96,8 +197,8 @@ void loop()
   Serial.println(val);             // debug value
   delay(100);
 }
-
 */
+
 
 /*
 //FOR MOTOR
